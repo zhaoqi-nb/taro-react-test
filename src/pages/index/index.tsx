63dacc12613c4414858bed0c946532ab8339365f
zhaoqi <qi.zhao@databurning.com>
2022-02-14 17:59:55 +0800

    项目初始化

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,44 @@
+import { Component } from 'react'
+import { Swiper, SwiperItem, View, CustomWrapper, Text } from '@tarojs/components'
+import './index.less'
+
+export default class Index extends Component {
+
+  componentWillMount() { }
+
+  componentDidMount() { }
+
+  componentWillUnmount() { }
+
+  componentDidShow() { }
+
+  componentDidHide() { }
+
+  render() {
+    return (
+      <Swiper
+        className='box'
+        autoplay
+        interval={1500}
+        indicatorColor='#999'
+        onClick={() => { }}
+        circular={true}
+        onAnimationFinish={() => { }}
+      >
+        <SwiperItem>
+          <View>
+            <CustomWrapper>
+              <Text>Hello, world!</Text>
+            </CustomWrapper>
+          </View>
+        </SwiperItem>
+        <SwiperItem>
+          <View className='text'>2</View>
+        </SwiperItem>
+        <SwiperItem>
+          <View className='text'>3</View>
+        </SwiperItem>
+      </Swiper>
+    )
+  }
+}
